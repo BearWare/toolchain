302b1e037a55d08659976716f1478d6264f1dfc4
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2020-03-22 20:40:57 +0100

    Use CMake toolchain provided by Android NDK

--- toolchain-android-x86.cmake
+++
@@ -1,8 +0,0 @@
-# Run 'cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-android-*.cmake' to
-# generate projects using this toolchain setup
-
-set (CMAKE_SYSTEM_NAME Android)
-set (CMAKE_SYSTEM_VERSION 21)
-set (CMAKE_ANDROID_ARCH_ABI x86)
-set (CMAKE_ANDROID_NDK $ENV{TOOLCHAIN_ROOT}/android-ndk-r17c)
-set (CMAKE_ANDROID_STL_TYPE gnustl_static)
