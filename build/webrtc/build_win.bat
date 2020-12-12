d1e968eae6c9dbbfea8329a40a4ca27a55ee0875
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2020-12-12 07:21:05 +0100

    Determine architecture from %Platform% and erase build output prior to generating new

--- build/webrtc/build_win.bat
+++ build/webrtc/build_win.bat
@@ -17,6 +17,7 @@
 @SET DEPOT_TOOLS_WIN_TOOLCHAIN=0
 @ECHO Adding depot_tools to PATH
 CD %WEBRTC_ROOT%\depot_tools
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 SET PATH=%CD%;%PATH%
 @ECHO ------------------------------------------------------------------
 @ECHO Cloning WebRTC is a slow process so here's a menu to skip steps.
@@ -31,37 +32,29 @@ SET PATH=%CD%;%PATH%
 @ECHO ------------------------------------------------------------------
 @ECHO Fetching WebRTC repository. This will take a long time.
 CD %WEBRTC_ROOT%
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 CALL fetch --nohooks webrtc
 
 :checkout
 @ECHO ------------------------------------------------------------------
 @ECHO Checking out branch
 CD %WEBRTC_ROOT%\src
-CALL git checkout branch-heads/4093
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
+CALL git clean -xdf .
+CALL git checkout modules/audio_processing/BUILD.gn
+CALL git checkout branch-heads/4332
 @ECHO Synchronizing checkout
 CALL gclient sync -D --with_branch_heads --with_tags
 @ECHO ------------------------------------------------------------------
 @ECHO Applying patch to build TeamTalk.lib
-CALL git checkout modules/audio_processing/BUILD.gn
-CALL git apply %INITWEBRTC%\libteamtalk-r4093.patch
+CALL git apply %INITWEBRTC%\libteamtalk-r4332.patch
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 
 :gengnfiles
 @ECHO ------------------------------------------------------------------
 @ECHO Set up Visual Studio version for WebRTC
-@CHOICE /C:12 /M "1 for VS2015, 2 for VS2019"
-@IF %ERRORLEVEL% == 1 GOTO vs2015
-@IF %ERRORLEVEL% == 2 GOTO vs2019
-:vs2015
-SET vs2015_install=%VSINSTALLDIR%
-@GOTO done
-:vs2019
-SET vs2019_install=%VSINSTALLDIR%
-@GOTO done
-
-:done
-@CHOICE /C:12 /M "1 for x86, 2 for x86_64"
-@IF %ERRORLEVEL% == 1 GOTO win32
-@IF %ERRORLEVEL% == 2 GOTO win64
+@IF %Platform% == x86 GOTO win32
+@IF %Platform% == x64 GOTO win64
 
 :win32
 @SET ARCH=x86
@@ -73,15 +66,22 @@ SET vs2019_install=%VSINSTALLDIR%
 :generate
 @ECHO ------------------------------------------------------------------
 CD %WEBRTC_ROOT%\src
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
+DEL /S /Q %INITWEBRTC%\debug
 CALL gn gen %INITWEBRTC%\debug --args="is_clang=false target_cpu=\"%ARCH%\" rtc_disable_logging=true rtc_exclude_field_trial_default=true rtc_enable_protobuf=false rtc_enable_sctp=false rtc_include_tests=false rtc_build_examples=false rtc_build_libvpx=false rtc_libvpx_build_vp9=false rtc_include_opus=false rtc_build_ssl=false rtc_builtin_ssl_root_certificates=false rtc_ssl_root=\"%TTLIBS_ROOT%\openssl\include\" is_debug=true use_custom_libcxx=false enable_iterator_debugging=true"
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 
+DEL /S /Q %INITWEBRTC%\release
 CALL gn gen %INITWEBRTC%\release --args="is_clang=false target_cpu=\"%ARCH%\" rtc_disable_logging=true rtc_exclude_field_trial_default=true rtc_enable_protobuf=false rtc_enable_sctp=false rtc_include_tests=false rtc_build_examples=false rtc_build_libvpx=false rtc_libvpx_build_vp9=false rtc_include_opus=false rtc_build_ssl=false rtc_builtin_ssl_root_certificates=false rtc_ssl_root=\"%TTLIBS_ROOT%\openssl\include\" is_debug=false use_custom_libcxx=false"
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 
 :build
 @ECHO ------------------------------------------------------------------
 CD %WEBRTC_ROOT%\src
 CALL ninja -v -C %INITWEBRTC%\debug teamtalk
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 CALL ninja -v -C %INITWEBRTC%\release teamtalk
+@IF NOT %ERRORLEVEL% == 0 GOTO buildfail
 
 :copyfiles
 CD %WEBRTC_ROOT%\src
@@ -92,3 +92,10 @@ XCOPY *.h /S %INITWEBRTC%\include
 @ECHO ------------------------------------------------------------------
 @ECHO Finishing building WebRTC for TeamTalk
 @ECHO You can now delete %WEBRTC_ROOT%
+@GOTO quit
+
+:buildfail
+@ECHO A build step failed
+
+:quit
+@CD %INITWEBRTC%
